9d3d9b3a524ca96db467817d00075457009d924d
Yunpeng Dong <58433153+YunpengDon@users.noreply.github.com>
2025-01-02 23:41:46 +0800

    complete exercise 9.10

--- Patientor/backend/src/index.ts
+++ Patientor/backend/src/index.ts
@@ -1,9 +1,9 @@
 import express from 'express';
 import cors from 'cors';
+import diagnosesRouter from './routes/diagnoses';
 const app = express();
 app.use(express.json());
-
-// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+ 
 app.use(cors());
 const PORT = 3001;
 
@@ -11,6 +11,9 @@ app.get('/api/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+// Create a type Diagnosis and use it to create endpoint /api/diagnoses for fetching all diagnoses with HTTP GET.
+app.use('/api/diagnoses', diagnosesRouter);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
