a2b078d0e11333998f74321172e84d7e84f83465
Yunpeng Dong <58433153+YunpengDon@users.noreply.github.com>
2025-01-08 20:48:08 +0800

    complete exercise 9.18

--- flight-diaries-FE/services/diaryService.ts
+++ flight-diaries-FE/services/diaryService.ts
@@ -1,9 +1,14 @@
 import axios from 'axios'
-import { DiaryEntry } from "../types";
+import { DiaryEntry, NewDiaryEntry } from "../types";
 
 const baseUrl = 'http://localhost:3000/api/diaries'
 
 export const getDiaries = async () => {
   const response = await axios.get<DiaryEntry[]>(baseUrl)
+  return response.data
+}
+
+export const addDiary = async (newDiary) => {
+  const response = await axios.post(baseUrl, newDiary)
   return response.data
 }
--- flight-diaries-FE/src/App.tsx
+++ flight-diaries-FE/src/App.tsx
@@ -1,38 +1,67 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import { getDiaries } from "../services/diaryService";
-import { DiaryEntry } from "../types";
-
+import { getDiaries, addDiary } from "../services/diaryService";
+import { Visibility, Weather, DiaryEntry } from "../types";
 
 function App() {
+  const [date, setDate] = useState('')
+  const [visibility, setVisibility] = useState('')
+  const [weather, setWeather] = useState('')
+  const [comment, setComment] = useState('')
   const [diries, setDiaries] = useState<DiaryEntry[]>([]);
 
   useEffect(() => {
-    getDiaries().then(data => {
+    getDiaries().then((data) => {
       console.log(data);
       setDiaries(data);
     });
   }, []);
+
+  const submitDiaryHandler = (event: React.SyntheticEvent) => {
+    event.preventDefault()
+    const newDiary = {
+      date,
+      visibility,
+      weather,
+      comment
+    } 
+    console.log(newDiary);
+    addDiary(newDiary)
+  }
+
   return (
     <div>
       <h2>Add new entry</h2>
-      <form>
-        <div>date <input type="text" /></div>
-        <div>visibility <input type="text" /></div>
-        <div>weather <input type="text" /></div>
-        <div>comment <input type="text" /></div>
+      <form onSubmit={submitDiaryHandler}>
+        <div>
+          date <input type="text" onChange={event => setDate(event.target.value)}/>
+        </div>
+        <div>
+          visibility <input type="text" onChange={event => setVisibility(event.target.value)}/>
+        </div>
+        <div>
+          weather <input type="text" onChange={event => setWeather(event.target.value)}/>
+        </div>
+        <div>
+          comment <input type="text" onChange={event => setComment(event.target.value)}/>
+        </div>
         <button type="submit">add</button>
       </form>
       <h2>Diary entries</h2>
-      {diries.map(diary => (
+      {diries.map((diary) => (
         <div key={diary.id}>
-        <h3>{diary.date}</h3>
-        <p>
-          visibility: {diary.visibility}
-          <br />
-          weather: {diary.weather}
-          {diary.comment ? <><br />comment: {diary.comment}</>: null}
-        </p>
+          <h3>{diary.date}</h3>
+          <p>
+            visibility: {diary.visibility}
+            <br />
+            weather: {diary.weather}
+            {diary.comment ? (
+              <>
+                <br />
+                comment: {diary.comment}
+              </>
+            ) : null}
+          </p>
         </div>
       ))}
     </div>
