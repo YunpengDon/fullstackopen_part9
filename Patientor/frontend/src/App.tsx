596a63c2ce1d69e0bedc7c4ee6dfc7b449986875
Yunpeng Dong <58433153+YunpengDon@users.noreply.github.com>
2025-01-10 23:16:59 +0800

    complete exercise 9.22

--- Patientor/frontend/src/App.tsx
+++ Patientor/frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
+import { Route, Link, Routes, useMatch, PathMatch } from "react-router-dom";
 import { Button, Divider, Container, Typography } from '@mui/material';
 
 import { apiBaseUrl } from "./constants";
@@ -8,9 +8,21 @@ import { Patient } from "./types";
 
 import patientService from "./services/patients";
 import PatientListPage from "./components/PatientListPage";
+import PatientDetail from "./components/PatientDetail";
 
 const App = () => {
   const [patients, setPatients] = useState<Patient[]>([]);
+  const [patient, setPatient] = useState<Patient|null>(null)
+  const match = useMatch('/patients/:id')
+  
+  const fetchPatientDetail = async (match: PathMatch<"id"> | null) => {
+    if ( match && match.params.id) {
+      const result = await patientService.getPatient(match.params.id)
+      result ? setPatient(result) : null
+    } else {
+      setPatient(null)
+    }
+  }
 
   useEffect(() => {
     void axios.get<void>(`${apiBaseUrl}/ping`);
@@ -20,11 +32,11 @@ const App = () => {
       setPatients(patients);
     };
     void fetchPatientList();
-  }, []);
+    fetchPatientDetail(match)
+  }, [match]);
   
   return (
     <div className="App">
-      <Router>
         <Container>
           <Typography variant="h3" style={{ marginBottom: "0.5em" }}>
             Patientor
@@ -35,9 +47,9 @@ const App = () => {
           <Divider hidden />
           <Routes>
             <Route path="/" element={<PatientListPage patients={patients} setPatients={setPatients} />} />
+            <Route path="patients/:id" element={<PatientDetail patient={patient}/>} />
           </Routes>
         </Container>
-      </Router>
     </div>
   );
 };
---
+++ Patientor/frontend/src/components/PatientDetail.tsx
@@ -0,0 +1,37 @@
+import Box from '@mui/material/Box';
+import Typography from '@mui/material/Typography';
+import FemaleIcon from '@mui/icons-material/Female';
+import MaleIcon from '@mui/icons-material/Male';
+
+import { Patient } from "../types";
+
+const PatientDetail = ({ patient }: { patient: Patient | null }) => {
+  
+  const getGenderIcon = (gender: string) => {
+    switch (gender.toLowerCase()) {
+      case 'male':
+        return <MaleIcon/>
+      case 'female':
+        return <FemaleIcon/>;
+      default:
+        return gender
+    }
+  }
+
+  if (patient) {
+    return (
+      <Box>
+        <Typography variant="h4" style={{ marginTop: "0.5em" }}>
+          {patient.name} {getGenderIcon(patient.gender)}
+        </Typography>
+        {patient.ssn ? <Typography variant='body1'>ssn: {patient.ssn}</Typography> : null}
+        <Typography variant='body1'>occupation: {patient.occupation}</Typography>
+      </Box>
+    );
+  } else {
+    // return <div>Error: Patient cannot be found.</div>;
+    return null
+  }
+};
+
+export default PatientDetail;
